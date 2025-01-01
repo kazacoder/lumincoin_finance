5e60c11498f98bec17ffb1e755d6901884933449
user <user@test.net>
2025-01-01 11:55:55 +0300

    Day 4 Sidebar completed.

--- webpack.config.js
+++ webpack.config.js
@@ -12,6 +12,9 @@ module.exports = {
             patterns: [
                 {from: "./node_modules/bootstrap/dist/css/bootstrap.min.css", to: "lib/bootstrap"},
                 {from: "./node_modules/bootstrap/dist/js/bootstrap.bundle.min.js", to: "lib/bootstrap"},
+                {from: "./node_modules/bootstrap-icons/font/fonts/bootstrap-icons.woff", to: "lib/bootstrap-icons/fonts"},
+                {from: "./node_modules/bootstrap-icons/font/fonts/bootstrap-icons.woff2", to: "lib/bootstrap-icons/fonts"},
+                {from: "./node_modules/bootstrap-icons/font/bootstrap-icons.min.css", to: "lib/bootstrap-icons"},
             ],
         }),
     ],
