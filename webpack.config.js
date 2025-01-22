3fa12d904bf0b2c1da4ffdb62ffe7c7495c33600
kazacoder <avtk.tlt@gmail.com>
2025-01-22 13:19:36 +0300

    Day 25. TerserPlugin installed and configured.

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const CopyPlugin = require("copy-webpack-plugin");
 const path = require('path');
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const Dotenv = require("dotenv-webpack");
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
     entry: './src/app.js',
@@ -69,4 +70,7 @@ module.exports = {
         }),
         new Dotenv(),
     ],
+    optimization: {
+        minimizer: [new TerserPlugin({ extractComments: false })],
+    },
 };
